b8682293a94e4ed37aa817fcf24c691d2d9bdcae
dingyaguang117 <dingyaguang117@gmail.com>
2013-08-12 09:34:13 +0800

    Create ThreadPool.rb

---
+++ ThreadPool.rb
@@ -0,0 +1,40 @@
+require "thread"
+
+
+class ThreadPool      
+    attr_accessor :max_size  
+
+    def initialize(max_size = 10)  
+      @max_size = max_size  
+      @q = SizedQueue.new(max_size)  
+    end
+
+    def process(&block)
+      @q.push(0)
+      Thread.new do
+        block.call
+        @q.pop
+      end
+    end
+end
+
+def test
+  pool = ThreadPool.new 10
+
+  (1..10000).each do |i|
+    pool.process do
+      puts "start #{i}"
+      sleep(3)
+      puts "end #{i}"
+    end
+    
+  end
+
+end
+
+
+
+
+if __FILE__ == $0
+  test
+end
